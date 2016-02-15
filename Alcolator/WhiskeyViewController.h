9aa1a19ea88ec39685bee529b2a0c2a4213782dc
Forrest Zhao <fozhao92@gmail.com>
2016-02-14 22:29:59 -0800

    Checkpoint 20 Connect Storyboad to Code: completed

---
+++ Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Forrest Zhao on 2/14/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
